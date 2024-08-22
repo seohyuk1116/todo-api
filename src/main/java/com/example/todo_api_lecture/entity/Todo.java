0cff2d8470deaa786411a3271ebb51a497a118d3
QWE03\user <tomnt1775@gmail.com>
2024-08-22 17:57:08 +0900

    no message

--- src/main/java/com/example/todo_api_lecture/entity/Todo.java
+++ src/main/java/com/example/todo_api_lecture/entity/Todo.java
@@ -4,8 +4,9 @@ package com.example.todo_api_lecture.entity;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
-
+@ToString
 @Setter
 @Getter
 @Entity
